b74cca72fb45f5477dc368d3b5a17bc64293648a
[SW4/2반] 오창현 <ckdgus5189@gmail.com>
2023-05-07 12:17:58 +0900

    반복되는 코드 컴포넌트화 시키고 map 돌리기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
+import { useState } from 'react';
+import data from './data';
 
 
 function App() {
+
+  const [shoes] = useState(data)
+
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
@@ -21,25 +26,28 @@ function App() {
 
       <Container>
         <Row>
-          <Col>
-            <img src='https://codingapple1.github.io/shop/shoes1.jpg' width="80%" />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-          <Col>
-            <img src='https://codingapple1.github.io/shop/shoes2.jpg' width="80%" />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-          <Col>
-            <img src='https://codingapple1.github.io/shop/shoes3.jpg' width="80%" />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
+          {
+            shoes.map((a, i) => {
+              return (
+                <Card shoes={shoes[i]} i={i} />
+              )
+            })
+          }
         </Row>
       </Container>
     </div>
   );
+}
+
+const Card = (props) => {
+  return (
+    <Col>
+      <img src={'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'} width="80%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content}</p>
+      <p>{props.shoes.price}</p>
+    </Col>
+  )
 }
 
 export default App;
