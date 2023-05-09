c8004f2ba7b3a044abfc267e4bc862dfdfe34471
ChanghyeonO <ckdgus5189@gmail.com>
2023-05-10 00:01:10 +0900

    docs : main 페이지 컴포넌트 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button, Navbar, Container, Nav, Row, Col } from "react-bootstrap";
-import { useState } from "react";
-import data from "./data";
+import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import Main from "./main";
 import Detail from "./detail";
 
 function App() {
-  const [shoes] = useState(data);
-
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
@@ -23,42 +21,12 @@ function App() {
       </Navbar>
 
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <div className="main-background"></div>
-              <Container>
-                <Row>
-                  {shoes.map((a, i) => {
-                    return <Card shoes={shoes[i]} i={i} />;
-                  })}
-                </Row>
-              </Container>
-            </>
-          }
-        />
+        <Route path="/" element={<Main />} />
         <Route path="/detail" element={<Detail />} />
         <Route />
       </Routes>
     </div>
   );
 }
-
-const Card = props => {
-  return (
-    <Col>
-      <img
-        src={
-          "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"
-        }
-        width="80%"
-      />
-      <h4>{props.shoes.title}</h4>
-      <p>{props.shoes.content}</p>
-      <p>{props.shoes.price}</p>
-    </Col>
-  );
-};
 
 export default App;
---
+++ src/main.jsx
@@ -0,0 +1,38 @@
+import { Button, Navbar, Container, Nav, Row, Col } from "react-bootstrap";
+import data from "./data";
+import React, { useState } from "react";
+
+const Main = () => {
+  const [shoes] = useState(data);
+
+  return (
+    <>
+      <div className="main-background"></div>
+      <Container>
+        <Row>
+          {shoes.map((a, i) => {
+            return <Card shoes={shoes[i]} i={i} />;
+          })}
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+const Card = props => {
+  return (
+    <Col>
+      <img
+        src={
+          "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"
+        }
+        width="80%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content}</p>
+      <p>{props.shoes.price}</p>
+    </Col>
+  );
+};
+
+export default Main;
